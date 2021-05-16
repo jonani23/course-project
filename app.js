6642ee73cefd51bced20196113f5884c8a625cfd
Okuk <jokuk@deloitte.com>
2021-05-16 13:28:11 -0400

    ejs files,partials and path modudles created

--- app.js
+++ app.js
@@ -9,26 +9,55 @@ const PORT = 3000;
 // Add Middleware
 app.use(morgan('combined'));
 
+// Add Path module
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+
+
 // Server
 app.listen(PORT, () =>{
     console.log(`The server is listening on port ${PORT}`);
 });
 
-// Create GET routes
-app.get("/", (request, response) => {
-    response.send("This route points to the Home page");
-})
-app.get("/about", (request, response) => {
-    response.send("This route points to the About page");
-})
-app.get("/login", (request, response) => {
-    response.send("This route points to the Login page");
-})
-app.get("/admin-console", (request, response) => {
-    response.send("This route points to the Admin Console page");
-})
-app.get("/admin-console/create-book", (request, response) => {
-    response.send("This route points to the Create page");
-})
+
+// Updating Route Handlers(using render)
+app.get('/', (request, respond) => {
+    respond.render('pages/index');
+});
+
+app.get('/about', (request, respond) => {
+    respond.render('pages/about')
+});
+
+app.get('/login', (request, respond) => {
+    respond.render('pages/login')
+});
+
+app.get('/admin-console', (request, respond) => {
+    respond.render('pages/admin')
+});
+
+app.get('/admin-console/create-book', (request, respond) => {
+    respond.render('pages/create');
+});
+
+
+// GET routes with Params
+app.get('/books/:id', (req, res) => {
+    res.render('pages/books');
+});
+
+app.get('/admin-console/update-book/:id', (req, res) => {
+    res.render('pages/update');
+
+});
+
+
+
+
+
+
 
 
